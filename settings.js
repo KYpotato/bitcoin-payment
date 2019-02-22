b0093e90b56740141da702c5190f8b051db43278
= <s0methingfun313@gmail.com>
2019-02-22 11:15:51 +0900

    change port number

--- settings.js
+++ settings.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-exports.port = 8080;
-//exports.port = process.env.PORT;
-=======
 exports.port = process.env.PORT;
->>>>>>> change port number
 exports.host = 'localhost';
 exports.slack_address = 'https://hooks.slack.com/services/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';
 exports.invoice_url = "http://localhost:3000/api/v1/invoice?amount=";
