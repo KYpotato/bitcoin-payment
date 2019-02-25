ab583618533267a354f4891d8408aecc3addcbe7
= <s0methingfun313@gmail.com>
2019-02-25 12:12:43 +0900

    moved DBURI to settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,10 @@
-//exports.port = process.env.PORT;
 exports.port = 8080;
+//exports.port = process.env.PORT;
 exports.host = 'localhost';
 exports.slack_address = 'https://hooks.slack.com/services/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';
 exports.invoice_url = "http://localhost:3000/api/v1/invoice?amount=";
 
+exports.mongodb_uri = "mongodb://" + this.host;
+//exports.mongodb_uri = process.env.MONGODB_URI;
 exports.orderdb = 'orderdb';
 exports.productdb = "productdb";
