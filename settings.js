bb214f36e84d7eb8c0dc65ceab6dec19427606ba
= <s0methingfun313@gmail.com>
2019-02-25 13:15:46 +0900

    mod db name

--- settings.js
+++ settings.js
@@ -5,10 +5,7 @@ exports.invoice_url = "http://localhost:3000/api/v1/invoice?amount=";
 
 //exports.mongodb_products_uri = "mongodb://" + this.host;
 exports.mongodb_products_uri = process.env.MONGODB_URI;
-exports.orderdb = 'orderdb';
+exports.orderdb = 'heroku_7bkqz5mr';
 //exports.mongodb_orders_uri = "mongodb://" + this.host;
 exports.mongodb_orders_uri = process.env.MONGOLAB_GRAY_URI;
-exports.productdb = "productdb";
-
-exports.check_tx_interval = 5 * 1000;
-exports.check_tx_timeout = 600 * 1000;
+exports.productdb = "heroku_s303mcbb";
