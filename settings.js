09bf15c1ec0f8297a47936949500017066cebac1
= <s0methingfun313@gmail.com>
2019-02-25 12:18:15 +0900

    mod setting

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ exports.host = 'localhost';
 exports.slack_address = 'https://hooks.slack.com/services/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';
 exports.invoice_url = "http://localhost:3000/api/v1/invoice?amount=";
 
-exports.mongodb_products_uri = "mongodb://" + this.host;
-exports.mongodb_orders_uri = "mongodb://" + this.host;
+//exports.mongodb_uri = "mongodb://" + this.host;
+exports.mongodb_uri = process.env.MONGODB_URI;
 exports.orderdb = 'orderdb';
 exports.productdb = "productdb";
 
