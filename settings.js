0da03268e36b4787a0273b27ccf583b147c62156
= <s0methingfun313@gmail.com>
2019-02-25 13:23:33 +0900

    move invoice url to environment variable

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 exports.port = process.env.PORT;
 exports.host = 'localhost';
 exports.slack_address = 'https://hooks.slack.com/services/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';
-exports.invoice_url = "http://localhost:3000/api/v1/invoice?amount=";
+exports.invoice_url = process.env.INVOICE_URL;
 
 //exports.mongodb_products_uri = "mongodb://" + this.host;
 exports.mongodb_products_uri = process.env.MONGODB_URI;
