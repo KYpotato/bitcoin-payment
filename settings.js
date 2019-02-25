ba8f32189c648aa926843b4f102890339e896027
= <s0methingfun313@gmail.com>
2019-02-25 14:36:28 +0900

    chenge timeout to 10min form 10sec

--- settings.js
+++ settings.js
@@ -11,4 +11,4 @@ exports.mongodb_orders_uri = process.env.MONGOLAB_GRAY_URI;
 exports.productdb = "heroku_s303mcbb";
 
 exports.check_tx_interval = 5 * 1000;
-exports.check_tx_timeout = 10 * 1000;
+exports.check_tx_timeout = 600 * 1000;
