791af87d8fef18662e749d93711f12b8e3a049a3
= <s0methingfun313@gmail.com>
2019-02-25 12:59:40 +0900

    mod for 2DBs(products db and orders db)

--- settings.js
+++ settings.js
@@ -3,9 +3,11 @@ exports.host = 'localhost';
 exports.slack_address = 'https://hooks.slack.com/services/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';
 exports.invoice_url = "http://localhost:3000/api/v1/invoice?amount=";
 
-//exports.mongodb_uri = "mongodb://" + this.host;
-exports.mongodb_uri = process.env.MONGODB_URI;
+//exports.mongodb_products_uri = "mongodb://" + this.host;
+exports.mongodb_products_uri = process.env.MONGODB_URI;
 exports.orderdb = 'orderdb';
+//exports.mongodb_orders_uri = "mongodb://" + this.host;
+exports.mongodb_orders_uri = process.env.MONGOLAB_GRAY_URI;
 exports.productdb = "productdb";
 
 exports.check_tx_interval = 5 * 1000;
