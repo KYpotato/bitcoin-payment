4c7d20cb92d7bcc851b9ec16ba54e0075289f7b0
= <s0methingfun313@gmail.com>
2019-03-04 18:49:12 +0900

    mod settings

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ exports.invoice_url = process.env.INVOICE_URL;
 
 exports.mongodb_products_uri = process.env.MONGODB_URI;
 exports.productdb = process.env.MONGODB_DBNAME;
-exports.mongodb_orders_uri = process.env.MONGOLAB_GRAY_URI;
-exports.orderdb = process.env.MONGOLAB_GRAY_DBNAME;
+exports.mongodb_orders_uri = process.env.MONGOLAB_NAVY_URI;
+exports.orderdb = process.env.MONGOLAB_NAVY_DBNAME;
 
 exports.check_tx_interval = 5 * 1000;
 exports.check_tx_timeout = 600 * 1000;
